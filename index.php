cf65d3616c3b50adc7a293e7079a0f3e634d94a7
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-10 21:19:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.ebay.com/itm/Alo-Yoga-Haze-Pullover-Sweatshirt-Womens/253203357719?var=552302843135&_trkparms=%26rpp_cid%3D5c2deb2c8888df492393fd54%26rpp_icid%3D5c334a2cf2c7cb696a248bea">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.ebay.com/itm/Alo-Yoga-Haze-Pullover-Sweatshirt-Womens/253203357719?var=552302843135&_trkparms=%26rpp_cid%3D5c2deb2c8888df492393fd54%26rpp_icid%3D5c334a2cf2c7cb696a248bea"
         </script>
         <title>Page Redirection</title>
     </head>
